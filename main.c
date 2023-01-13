489c3656d7bca06d61eb82fba3fc6d23a935e761
NatanaelWT <Natanaelhdxd25@gmail.com>
2023-01-13 08:59:08 +0700

    Daftar Menu

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 
 int main() {
+    while (1)
+    {
+      printf("Pilih Menu :");
+      printf("1. Lihat Data Dosen");
+      printf("2. Input Data Dosen");
+      printf("3. Edit Data Dosen");
+      printf("4. Hapus Data Dosen");
+      
+    }
     
   return 0;
 }
