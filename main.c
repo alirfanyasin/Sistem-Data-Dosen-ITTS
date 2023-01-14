d0a13181f3b10a00d787d3c37307e5eecf142e29
NatanaelWT <Natanaelhdxd25@gmail.com>
2023-01-14 19:03:19 +0700

    Update Lihat Data

--- main.c
+++ main.c
@@ -12,50 +12,81 @@ struct dosen{
 
 void lihatDataDosen(){
     char dataDosen[500];
-    struct dosen lihatData;
+    struct dosen lihatData[100];
     FILE *fptr;
     char *hasil;
-    int index[4];
-
-  fptr = fopen("dataDosen.txt", "r");
-  while (fgets(dataDosen, sizeof(dataDosen), fptr)){
+    
+    fptr = fopen("dataDosen.txt", "r");
+      int loop = 0;
+      while (fgets(dataDosen, sizeof(dataDosen), fptr)){
         hasil = strchr(dataDosen, '|');
-        int loop = 0;
-        while(hasil != NULL){
-            index[loop] = hasil - dataDosen;
-            printf("Ditemukan pada huruf ke-%d\n", hasil - dataDosen);
-            hasil = strchr(hasil+1, '|');
+        int loop2 = 0;
+        int index[4];
+          while(hasil != NULL){
+              index[loop2] = hasil - dataDosen;
+              hasil = strchr(hasil+1, '|');
+              loop2++;
+          }
+
+              for (int j = 0; j < index[0]; j++)
+              {
+                lihatData[loop].nip[j] = dataDosen[j];
+              }
+              for (int j = 0; j < index[1]-index[0]-1; j++)
+              {
+                lihatData[loop].nama[j] = dataDosen[j+index[0]+1];
+              }
+              for (int j = 0; j < index[2]-index[1]-1; j++)
+              {
+                lihatData[loop].matkul[j] = dataDosen[j+index[1]+1];
+              }
+              for (int j = 0; j < index[3]-index[2]-1; j++)
+              {
+                lihatData[loop].alamat[j] = dataDosen[j+index[2]+1];
+              }
+              for (int j = 0; j < strlen(dataDosen)-index[3]-1; j++)
+              {
+                lihatData[loop].kontak[j] = dataDosen[j+index[3]+1];
+              }
+            printf("\n");  
+            printf("NIP = %s", lihatData[loop].nip);
+            printf("\n");
+            printf("Nama = %s", lihatData[loop].nama);
+            printf("\n");
+            printf("Matkul = %s", lihatData[loop].matkul);
+            printf("\n");
+            printf("Alamat = %s", lihatData[loop].alamat);
+            printf("\n");
+            printf("Kontak = %s", lihatData[loop].kontak);
+            printf("\n");
             loop++;
-            printf("%d", index[loop]);
-        }
-        printf("\n");
-    }
+      }
   fclose(fptr);
 }
 
 void inputDataDosen(){
-  struct dosen dosen01;
+  struct dosen inputDosen;
   printf("\nNIP : ");
-  scanf(" %[^\n]s", dosen01.nip);
+  scanf(" %[^\n]s", inputDosen.nip);
   printf("Nama : ");
-  scanf(" %[^\n]s", dosen01.nama);
+  scanf(" %[^\n]s", inputDosen.nama);
   printf("Matkul : ");
-  scanf(" %[^\n]s", dosen01.matkul);
+  scanf(" %[^\n]s", inputDosen.matkul);
   printf("Alamat : ");
-  scanf(" %[^\n]s", dosen01.alamat);
+  scanf(" %[^\n]s", inputDosen.alamat);
   printf("Kontak : ");
-  scanf(" %[^\n]s", dosen01.kontak);
+  scanf(" %[^\n]s", inputDosen.kontak);
 
   char str[500];
-  strcat(str, dosen01.nip);
+  strcat(str, inputDosen.nip);
   strcat(str, "|");
-  strcat(str, dosen01.nama);
+  strcat(str, inputDosen.nama);
   strcat(str, "|");
-  strcat(str, dosen01.matkul);
+  strcat(str, inputDosen.matkul);
   strcat(str, "|");
-  strcat(str, dosen01.alamat);
+  strcat(str, inputDosen.alamat);
   strcat(str, "|");
-  strcat(str, dosen01.kontak);
+  strcat(str, inputDosen.kontak);
   strcat(str, "\n");
 
   FILE *fptr;
